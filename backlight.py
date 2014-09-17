f37f5305020041f3c87641a445ec8bf7d5ad0a3d
Helge Jung <helge.jung@youngage.eu>
2014-09-17 21:16:49 +0200

    rework according to Python styleguides

--- backlight.py
+++ backlight.py
@@ -7,54 +7,53 @@
 from smbus import SMBus
 
 class Backlight(object):
-	REG_RED		= 0x04 # pwm2
-	REG_GREEN	= 0x03 # pwm1
-	REG_BLUE	= 0x02 # pwm0
+    REG_RED = 0x04 # pwm2
+    REG_GREEN = 0x03 # pwm1
+    REG_BLUE = 0x02 # pwm0
 
-	REG_MODE1	= 0x00
-	REG_MODE2	= 0x01
-	REG_OUTPUT	= 0x08
+    REG_MODE1 = 0x00
+    REG_MODE2 = 0x01
+    REG_OUTPUT = 0x08
 
-	def __init__(self, bus, address):
-		if not isinstance(bus, SMBus):
-			raise TypeError
+    def __init__(self, bus, address):
+        if not isinstance(bus, SMBus):
+            raise TypeError
 
-		self.bus = bus
-		self.address = int(address)
+        self.bus = bus
+        self.address = int(address)
 
-		# initialize
-		self.setRegister(self.REG_MODE1, 0)
-		self.setRegister(self.REG_MODE2, 0)
+        # initialize
+        self.set_register(self.REG_MODE1, 0)
+        self.set_register(self.REG_MODE2, 0)
 
-		# all LED control by PWM
-		self.setRegister(self.REG_OUTPUT, 0xAA)
+        # all LED control by PWM
+        self.set_register(self.REG_OUTPUT, 0xAA)
 
-	def setRegister(self, addr, value):
-		self.bus.write_byte_data(self.address, addr, value)
+    def set_register(self, addr, value):
+        self.bus.write_byte_data(self.address, addr, value)
 
-	def setColor(self, red, green, blue):
-		r = int(red)
-		g = int(green)
-		b = int(blue)
-		self.setRegister(self.REG_RED, r)
-		self.setRegister(self.REG_GREEN, g)
-		self.setRegister(self.REG_BLUE, b)
-
+    def set_color(self, red, green, blue):
+        r = int(red)
+        g = int(green)
+        b = int(blue)
+        self.set_register(self.REG_RED, r)
+        self.set_register(self.REG_GREEN, g)
+        self.set_register(self.REG_BLUE, b)
 
 if __name__ == '__main__':
-	import time
+    import time
 
-	bus = SMBus(1)
-	light = Backlight(bus, 0x62)
+    bus = SMBus(1)
+    light = Backlight(bus, 0x62)
 
-	light.setColor(255, 0, 0)
-	time.sleep(1)
-	light.setColor(0,255,0)
-	time.sleep(1)
-	light.setColor(0,0,255)
-	time.sleep(1)
-	light.setColor(255,0,255)
-	time.sleep(1)
-	light.setColor(255,255,255)
-	time.sleep(1)
-	light.setColor(0, 0, 128)
+    light.set_color(255, 0, 0)
+    time.sleep(1)
+    light.set_color(0,255,0)
+    time.sleep(1)
+    light.set_color(0,0,255)
+    time.sleep(1)
+    light.set_color(255,0,255)
+    time.sleep(1)
+    light.set_color(255,255,255)
+    time.sleep(1)
+    light.set_color(0, 0, 128)
--- display.py
+++ display.py
@@ -8,118 +8,118 @@ from smbus import SMBus
 import time
 
 class Display(object):
-	# commands
-	LCD_CLEARDISPLAY = 0x01
-	LCD_RETURNHOME = 0x02
-	LCD_ENTRYMODESET = 0x04
-	LCD_DISPLAYCONTROL = 0x08
-	LCD_CURSORSHIFT = 0x10
-	LCD_FUNCTIONSET = 0x20
-	LCD_SETCGRAMADDR = 0x40
-	LCD_SETDDRAMADDR = 0x80
+    # commands
+    LCD_CLEARDISPLAY = 0x01
+    LCD_RETURNHOME = 0x02
+    LCD_ENTRYMODESET = 0x04
+    LCD_DISPLAYCONTROL = 0x08
+    LCD_CURSORSHIFT = 0x10
+    LCD_FUNCTIONSET = 0x20
+    LCD_SETCGRAMADDR = 0x40
+    LCD_SETDDRAMADDR = 0x80
 
-	# flags for display entry mode
-	LCD_ENTRYRIGHT = 0x00
-	LCD_ENTRYLEFT = 0x02
-	LCD_ENTRYSHIFTINCREMENT = 0x01
-	LCD_ENTRYSHIFTDECREMENT = 0x00
+    # flags for display entry mode
+    LCD_ENTRYRIGHT = 0x00
+    LCD_ENTRYLEFT = 0x02
+    LCD_ENTRYSHIFTINCREMENT = 0x01
+    LCD_ENTRYSHIFTDECREMENT = 0x00
 
-	# flags for display on/off control
-	LCD_DISPLAYON = 0x04
-	LCD_DISPLAYOFF = 0x00
-	LCD_CURSORON = 0x02
-	LCD_CURSOROFF = 0x00
-	LCD_BLINKON = 0x01
-	LCD_BLINKOFF = 0x00
+    # flags for display on/off control
+    LCD_DISPLAYON = 0x04
+    LCD_DISPLAYOFF = 0x00
+    LCD_CURSORON = 0x02
+    LCD_CURSOROFF = 0x00
+    LCD_BLINKON = 0x01
+    LCD_BLINKOFF = 0x00
 
-	# flags for display/cursor shift
-	LCD_DISPLAYMOVE = 0x08
-	LCD_CURSORMOVE = 0x00
-	LCD_MOVERIGHT = 0x04
-	LCD_MOVELEFT = 0x00
+    # flags for display/cursor shift
+    LCD_DISPLAYMOVE = 0x08
+    LCD_CURSORMOVE = 0x00
+    LCD_MOVERIGHT = 0x04
+    LCD_MOVELEFT = 0x00
 
-	# flags for function set
-	LCD_8BITMODE = 0x10
-	LCD_4BITMODE = 0x00
-	LCD_2LINE = 0x08
-	LCD_1LINE = 0x00
-	LCD_5x10DOTS = 0x04
-	LCD_5x8DOTS = 0x00
+    # flags for function set
+    LCD_8BITMODE = 0x10
+    LCD_4BITMODE = 0x00
+    LCD_2LINE = 0x08
+    LCD_1LINE = 0x00
+    LCD_5x10DOTS = 0x04
+    LCD_5x8DOTS = 0x00
 
-	def __init__(self, bus, address, oneline=False, charsize=LCD_5x8DOTS):
-		if not isinstance(bus, SMBus):
-			raise TypeError
+    def __init__(self, bus, address, oneline=False, charsize=LCD_5x8DOTS):
+        if not isinstance(bus, SMBus):
+            raise TypeError
 
-		self.bus = bus
-		self.address = int(address)
+        self.bus = bus
+        self.address = int(address)
 
-		self.disp_func = self.LCD_DISPLAYON # | 0x10
-		if not oneline:
-			self.disp_func |= self.LCD_2LINE
-		elif charsize != 0:
-			# for 1-line displays you can choose another dotsize
-			self.disp_func |= self.LCD_5x10DOTS
+        self.disp_func = self.LCD_DISPLAYON # | 0x10
+        if not oneline:
+            self.disp_func |= self.LCD_2LINE
+        elif charsize != 0:
+            # for 1-line displays you can choose another dotsize
+            self.disp_func |= self.LCD_5x10DOTS
 
-		# wait for display init after power-on
-		time.sleep(0.050) # 50ms
+        # wait for display init after power-on
+        time.sleep(0.050) # 50ms
 
-		# send function set
-		self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
-		time.sleep(0.005) ##time.sleep(0.0045) # 4.5ms
-		self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
-		time.sleep(0.001) ##time.sleep(0.000150) # 150µs = 0.15ms
-		self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
-		self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
- 
-		# turn on the display
-		self.disp_ctrl = self.LCD_DISPLAYON | self.LCD_CURSOROFF | self.LCD_BLINKOFF
-		self.display()
+        # send function set
+        self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
+        time.sleep(0.005) ##time.sleep(0.0045) # 4.5ms
+        self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
+        time.sleep(0.001) ##time.sleep(0.000150) # 150µs = 0.15ms
+        self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
+        self.cmd(self.LCD_FUNCTIONSET | self.disp_func)
 
-		# clear it
-		self.clear()
+        # turn on the display
+        self.disp_ctrl = self.LCD_DISPLAYON | self.LCD_CURSOROFF | self.LCD_BLINKOFF
+        self.display()
 
-		# set default text direction (left-to-right)
-		self.disp_mode = self.LCD_ENTRYLEFT | self.LCD_ENTRYSHIFTDECREMENT
-		self.cmd(self.LCD_ENTRYMODESET | self.disp_mode)
+        # clear it
+        self.clear()
 
-	def cmd(self, command):
-		assert command >= 0 and command < 256
-		self.bus.write_byte(self.address, 0x80)
-		self.bus.write_byte(self.address, command)
-		print 'cmd %x' % command
+        # set default text direction (left-to-right)
+        self.disp_mode = self.LCD_ENTRYLEFT | self.LCD_ENTRYSHIFTDECREMENT
+        self.cmd(self.LCD_ENTRYMODESET | self.disp_mode)
 
-	def writeChar(self, c):
-		assert c >= 0 and c < 256
-		print 'write char %x' % c
-		self.bus.write_byte(self.address, 0x40)
-		self.bus.write_byte(self.address, c)
+    def cmd(self, command):
+        assert command >= 0 and command < 256
+        self.bus.write_byte(self.address, 0x80)
+        self.bus.write_byte(self.address, command)
+        print 'cmd %x' % command
 
-	def write(self, s):
-		for c in s:
-			self.writeChar(ord(c))
+    def write_char(self, c):
+        assert c >= 0 and c < 256
+        print 'write char %x' % c
+        self.bus.write_byte(self.address, 0x40)
+        self.bus.write_byte(self.address, c)
 
-	def display(self):
-		self.disp_ctrl |= self.LCD_DISPLAYON
-		self.cmd(self.LCD_DISPLAYCONTROL | self.disp_ctrl)
+    def write(self, text):
+        for char in text:
+            self.write_char(ord(char))
 
-	def nodisplay(self):
-		self.disp_ctrl &= ~self.LCD_DISPLAYON
-		self.cmd(self.LCD_DISPLAYCONTROL | self.disp_ctrl)
+    def display(self):
+        self.disp_ctrl |= self.LCD_DISPLAYON
+        self.cmd(self.LCD_DISPLAYCONTROL | self.disp_ctrl)
 
-	def clear(self):
-		self.cmd(self.LCD_CLEARDISPLAY)
-		time.sleep(0.002) # 2ms
+    def nodisplay(self):
+        self.disp_ctrl &= ~self.LCD_DISPLAYON
+        self.cmd(self.LCD_DISPLAYCONTROL | self.disp_ctrl)
 
-	def home(self):
-		self.cmd(self.LCD_RETURNHOME)
-		time.sleep(0.002) # 2ms
+    def clear(self):
+        self.cmd(self.LCD_CLEARDISPLAY)
+        time.sleep(0.002) # 2ms
+
+    def home(self):
+        self.cmd(self.LCD_RETURNHOME)
+        time.sleep(0.002) # 2ms
 
 if __name__ == '__main__':
-	import time
+    import time
 
-	bus = SMBus(1)
-	d = Display(bus, 0x3E)
+    bus = SMBus(1)
+    d = Display(bus, 0x3E)
 
-	d.home()
-	d.write('Hallo Welt')
+    d.home()
+    d.write('Hallo Welt')
 
